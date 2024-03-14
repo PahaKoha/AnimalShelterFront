72ae98eb8145f9e17d5f577903553db8e1557623
paha_koha <pashagerasimik@mail.ru>
2024-03-14 15:52:34 +0300

    Инжектировал create-new-pet-window.service в admin-page.component, чтобы можно было управлять состоянием компонента create-new-pet-window.component на странице администтратора.

--- src/app/components/admin-page/admin-page.component.ts
+++ src/app/components/admin-page/admin-page.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CreateNewPetWindowService} from "../../services/create-new-pet-window.service";
+import {CreateNewPetWindowComponent} from "../create-new-pet-window/create-new-pet-window.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CreateNewPetWindowComponent,
+    NgIf
+  ],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.css'
 })
 export class AdminPageComponent {
+  constructor(private createNewPetWindowService: CreateNewPetWindowService) {
+  }
 
+  isCreateNewPetWindowOpen(): boolean {
+    return this.createNewPetWindowService.isWindowOpen();
+  }
+
+  changeCreateNewPetWindowState(): void {
+    this.createNewPetWindowService.changeWindowState();
+  }
 }
