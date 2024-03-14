0f496e791caab098fde213d2abbb48888d9a77ce
paha_koha <pashagerasimik@mail.ru>
2024-03-14 13:10:27 +0300

    Добавил компонент для добавления нового питомца

---
+++ src/app/components/create-new-pet-window/create-new-pet-window.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-pet-window',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-new-pet-window.component.html',
+  styleUrl: './create-new-pet-window.component.css'
+})
+export class CreateNewPetWindowComponent {
+
+}
