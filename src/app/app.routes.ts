745cfa9ec3f811bfaba666124e382222d5364ce7
paha_koha <pashagerasimik@mail.ru>
2024-03-14 13:06:33 +0300

    Добавил новый путь для страницы админа и кнопку для вызова окна создания нового питомца на странице админа

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {AdminPageComponent} from "./components/admin-page/admin-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "admin", component: AdminPageComponent}
+];
