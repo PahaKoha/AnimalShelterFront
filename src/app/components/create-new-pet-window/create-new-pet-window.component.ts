70cd94781d1ac905292675741a820a01573ccc3d
paha_koha <pashagerasimik@mail.ru>
2024-03-15 23:03:29 +0300

    Добавил новый formGroup для окна создания питомца. Создал тестовый метод для проверки введенных в форму данных.

--- src/app/components/create-new-pet-window/create-new-pet-window.component.ts
+++ src/app/components/create-new-pet-window/create-new-pet-window.component.ts
@@ -1,18 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CreateNewPetWindowService} from "../../services/create-new-pet-window.service";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-new-pet-window',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './create-new-pet-window.component.html',
   styleUrl: './create-new-pet-window.component.css'
 })
-export class CreateNewPetWindowComponent {
-  constructor(private createNewPetWindowService: CreateNewPetWindowService) {
+export class CreateNewPetWindowComponent implements OnInit {
+  constructor(private createNewPetWindowService: CreateNewPetWindowService, private formBuilder: FormBuilder) {
+  }
+
+  newPetFormGroup!: FormGroup;
+
+  ngOnInit(): void {
+    this.newPetFormGroup = this.formBuilder.group({
+      name: ['', Validators.required],
+      age: ['', Validators.required],
+      sex: ['', Validators.required],
+      petPicture: ['', Validators.required]
+    })
   }
 
   changeCreateNewPetWindowState(): void {
     this.createNewPetWindowService.changeWindowState();
+  }
+
+  showInfoAboutPet(): void {
+    console.log(this.newPetFormGroup.value);
   }
 }
