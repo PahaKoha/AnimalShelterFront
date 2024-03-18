92959522b638e12efe7dd44ef9edf8082ec33fef
ArtgtH <artem2004920@gmail.com>
2024-03-18 20:42:07 +0300

    добавил компонент для удаления животного по id в родительский компонент

--- src/app/components/admin-page/admin-page.component.ts
+++ src/app/components/admin-page/admin-page.component.ts
@@ -2,19 +2,30 @@ import {Component} from '@angular/core';
 import {CreateNewPetWindowService} from "../../services/create-new-pet-window.service";
 import {CreateNewPetWindowComponent} from "../create-new-pet-window/create-new-pet-window.component";
 import {NgIf} from "@angular/common";
+import {DeletePetWindowComponent} from "../delete-pet-window/delete-pet-window.component";
+import {DeletePetWindowService} from "../../services/delete-pet-window.service";
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
   imports: [
     CreateNewPetWindowComponent,
-    NgIf
+    NgIf,
+    DeletePetWindowComponent
   ],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.css'
 })
 export class AdminPageComponent {
-  constructor(private createNewPetWindowService: CreateNewPetWindowService) {
+  constructor(private createNewPetWindowService: CreateNewPetWindowService, private deletePetWindow: DeletePetWindowService) {
+  }
+
+  isCreateDeletePetWindowOpen(): boolean {
+    return this.deletePetWindow.isWindowOpen();
+  }
+
+  changeDeleteWindowState(): void {
+    return this.deletePetWindow.changeWindowState();
   }
 
   isCreateNewPetWindowOpen(): boolean {
