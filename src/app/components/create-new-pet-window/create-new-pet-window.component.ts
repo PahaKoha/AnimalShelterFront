e989b30d2c9ea6e135572eaede858c71854ff140
ArtgtH <artem2004920@gmail.com>
2024-03-29 20:47:26 +0300

    добавление фотографии и её вывод на страницу

--- src/app/components/create-new-pet-window/create-new-pet-window.component.ts
+++ src/app/components/create-new-pet-window/create-new-pet-window.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {CreateNewPetWindowService} from "../../services/create-new-pet-window.service";
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-create-new-pet-window',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgOptimizedImage,
+    NgIf
   ],
   templateUrl: './create-new-pet-window.component.html',
   styleUrl: './create-new-pet-window.component.css'
@@ -16,6 +19,8 @@ export class CreateNewPetWindowComponent implements OnInit {
   }
 
   newPetFormGroup!: FormGroup;
+  imageUrl: string | undefined;
+
 
   ngOnInit(): void {
     this.newPetFormGroup = this.formBuilder.group({
@@ -32,5 +37,14 @@ export class CreateNewPetWindowComponent implements OnInit {
 
   showInfoAboutPet(): void {
     console.log(this.newPetFormGroup.value);
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.imageUrl = e.target.result;
+    };
+    reader.readAsDataURL(file);
   }
 }
